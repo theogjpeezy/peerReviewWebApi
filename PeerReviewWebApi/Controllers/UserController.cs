1e88f524666b67553462dc34df5363e56ef84fff
theogjpeezy <justin.cronin01@gmail.com>
2015-12-10 15:51:37 -0500

    Adding CORS crap

--- PeerReviewWebApi/Controllers/UserController.cs
+++ PeerReviewWebApi/Controllers/UserController.cs
@@ -6,11 +6,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Practices.EnterpriseLibrary.Data;
 using PeerReviewWebApi.Models;
 
 namespace PeerReviewWebApi.Controllers
 {
+    [EnableCors("*", "*", "*")]
 	public class UserController : ApiController
     {
 		static readonly IUserRepository UserRepo = new UserRepository();
